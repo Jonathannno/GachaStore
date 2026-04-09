7f19bf7a9efd94e63137388ec9dc29bfe7a4ac6a
Jonathannno <tson54531@gmail.com>
2026-04-09 20:59:06 +0700

    Fixing DB connection variables

--- src/java/model/DBConnect.java
+++ src/java/model/DBConnect.java
@@ -9,13 +9,18 @@ public class DBConnect {
         Connection conn = null;
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            // This pulls the info from the Railway Variables you added in Step 1
+            // If it can't find them (like when you run it on your laptop), it uses "localhost"
+            String host = System.getenv("MYSQLHOST") != null ? System.getenv("MYSQLHOST") : "localhost";
+            String port = System.getenv("MYSQLPORT") != null ? System.getenv("MYSQLPORT") : "3306";
+            String dbName = System.getenv("MYSQLDATABASE") != null ? System.getenv("MYSQLDATABASE") : "loginwebshop";
+            String user = System.getenv("MYSQLUSER") != null ? System.getenv("MYSQLUSER") : "root";
+            String pass = System.getenv("MYSQLPASSWORD") != null ? System.getenv("MYSQLPASSWORD") : "";
 
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/loginwebshop",
-                    "root",
-                    ""
-            );
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, pass);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -23,6 +28,4 @@ public class DBConnect {
 
         return conn;
     }
-     
-
 }
